37b80222351d413011993ad226772eb25d21774a
Dennis Telezhnikov <mr.kymman@gmail.com>
2018-03-15 01:00:20 +0300

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Timer from './Timer';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Timer />, document.getElementById('timer'));
-registerServiceWorker();
